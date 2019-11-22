fbd9be4f33f58f134643732347929ac3cb746d9b
manu <napestershine@gmail.com>
2019-11-22 22:18:24 +0100

    git init

---
+++ components/StoryList.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+const StoryList = ({stories}) => (
+    <div className="story-list">
+        {stories.map(story => (
+            <div className="story" key={story.id}>
+                <h2 className="story-title">
+                    <a href={story.url}>{story.title}</a>
+                </h2>
+                <div className="story-details">
+                    <span>{story.points || 0} points</span>
+                    <Link href={`/story?id=${story.id}`}>
+                        <a>{story.comments_count || 0}</a>
+                    </Link>
+                </div>
+            </div>
+        ))}
+    </div>
+);
+
+export default StoryList;
